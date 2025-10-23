57fe059e6c596d4b72b2f2963f5f6b70f015134a
Joshua Calibo <68768061+awsuj@users.noreply.github.com>
2025-10-23 16:19:07 +0800

    Update Main.kt

--- kotlin/Main.kt
+++ kotlin/Main.kt
@@ -1,5 +1,13 @@
 import com.bankingapp.*
 
+/*
+********************
+Last names: Calibo, Nono, Panlilio, So
+Language: Kotlin
+Paradigm(s): Object-Oriented Programming
+********************
+*/
+
 fun main() {
     val forex: MoneyChanger = MoneyChanger()
     val controller: Controller = Controller(forex)
